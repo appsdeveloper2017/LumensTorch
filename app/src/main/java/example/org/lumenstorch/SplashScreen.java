e3fa015adf886d84224a165c580ab0487083ca3d
Alex Fernandez <alexfernandez@MacBook-Pro-de-Alex.local>
2018-02-03 19:01:16 +0100

    Splash screen en lumens torch.

---
+++ app/src/main/java/example/org/lumenstorch/SplashScreen.java
@@ -0,0 +1,70 @@
+package example.org.lumenstorch;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private TextView textView;
+    private ImageView imageSplash;
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        textView = (TextView)findViewById(R.id.titulo_splash);
+
+        mContext = this;
+
+        init();
+        animateOnCreateViews();
+
+    }
+
+    private void init() {
+        imageSplash = (ImageView) findViewById(R.id.imagen_splash);
+    }
+
+    private void animateOnCreateViews() {
+        Animation animation = AnimationUtils.loadAnimation(mContext, R.anim.animation_splash_icon);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                splashScrennDelay();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        imageSplash.startAnimation(animation);
+    }
+
+    private void splashScrennDelay() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashScreen.this, FrontTorch.class);
+                startActivity(intent);
+
+            }
+        }, 1500);
+
+    }
+}
